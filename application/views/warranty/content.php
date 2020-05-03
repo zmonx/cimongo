d47a1a7e404c4e419fea9d371f5d68bbbcf79d1c
zmonx <chumpitak@gmail.com>
2020-05-04 02:17:42 +0700

    fix bug

--- application/views/warranty/content.php
+++ application/views/warranty/content.php
@@ -1,6 +1,6 @@
 <div class="home">
     <div class="home_container">
-        <div class="home_background" style="background-image:url(<?php echo base_url('public/images/contact.jpg') ?>)"></div>
+        <div class="home_background" style="background-image:url(<?php echo base_url('public/images/contact.jpg')?>)"></div>
         <div class="home_content_container">
             <div class="container">
                 <div class="row">
@@ -8,7 +8,7 @@
                         <div class="home_content">
                             <div class="breadcrumbs">
                                 <ul>
-                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="<?php echo base_url('home')?>">Home</a></li>
                                     <li>Warranty</li>
                                 </ul>
                             </div>
@@ -23,7 +23,8 @@
     <div class="container">
         <div class="txtwar">
 
-            <b>เงื่อนไขการรับประกันสินค้า</b> <br>
+            <b>เงื่อนไขการรับประกันสินค้า</b>
+            <br>
             <h4>_____________________</h4>
         </div>
         <div class="img_warranty">
@@ -33,49 +34,48 @@
             <img src="<?php echo base_url('public/images/warranty4.webp') ?>" alt="">
             <img src="<?php echo base_url('public/images/warranty5.webp') ?>" alt="">
             <img src="<?php echo base_url('public/images/warranty6.webp') ?>" alt="">
-            <img src="<?php echo base_url('public/images/warranty7.webp') ?>" alt="">
-            <br><br>
+            <img src="<?php echo base_url('public/images/warranty7.jpg') ?>" alt="">
+            <br>
+            <br>
         </div>
     </div>
 </div>
 
-<!-- Footer -->
-
-<div class="footer_overlay">
-    <footer class="footer">
-        <div class="footer_background" style="background-image:url(<?php echo base_url('public/images/footer.jpg') ?>)"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="footer_content d-flex flex-lg-row flex-column align-items-center justify-content-lg-start justify-content-center">
-                        <div class="footer_logo"><a href="#">EAZY!T</a></div>
-                        <div class="copyright ml-auto mr-auto">
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;<script>
-                                document.write(new Date().getFullYear());
-                            </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="<?php echo base_url('https://colorlib.com') ?>" target="_blank">Colorlib</a>
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </div>
-                        <div class="footer_social ml-lg-auto">
-                            <ul>
-                                <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                            </ul>
-                        </div>
+<div class="footer_overlay"></div>
+<footer class="footer">
+    <div class="footer_background" style="background-image:url(<?php echo base_url('public/images/footer.jpg') ?>)"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="footer_content d-flex flex-lg-row flex-column align-items-center justify-content-lg-start justify-content-center">
+                    <div class="footer_logo"><a href="#">EAZY!T</a></div>
+                    <div class="copyright ml-auto mr-auto">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | Thank you so much<i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </div>
+                    <div class="footer_social ml-lg-auto">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
 </div>
 <style>
     .img_warranty {
         text-align: center;
     }
-
+    
     .txtwar {
         color: black;
     }
-</style>
